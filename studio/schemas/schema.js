506797fe617f7daee0641b8628b16b9b0a3a02d5
Ben C. Zheng <benczheng@users.noreply.github.com>
2020-06-19 17:31:32 -0700

    Schema change

--- studio/schemas/schema.js
+++ studio/schemas/schema.js
@@ -31,7 +31,7 @@ import link from './objects/link'
 import variation from './objects/variation'
 import openGraph from './objects/openGraph'
 import latex from './latex'
-import feature
+
 
 const allPlugs = Object.values(plugs).map((plug) => {
   return { ...plug, fields: plugDefaultFields.concat(plug.fields) }
